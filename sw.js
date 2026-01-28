d9fba6a917cd653dae28a4df7dad7cc8a4e6551c
siyabongayawa5-afk <siyabongayawa5@gmail.com>
2026-01-29 01:24:46 +0200

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'printer-dashboard-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/offline.html'))
+  );
+});
